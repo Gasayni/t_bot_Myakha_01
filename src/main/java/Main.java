b51e60aef150b5b4435ad31742e5bab47452a67d
Gasayni <gasajni@gmail.com>
2022-02-16 14:49:14 +0300

    WebHook (Не работает)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.ApiConstants;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.Webhook;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import org.telegram.telegrambots.updatesreceivers.DefaultWebhook;
-
-import javax.validation.groups.Default;
 
 public class Main {
     public static void main(String[] args) {
